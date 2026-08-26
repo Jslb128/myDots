721acb292d426327c146d84af1523ec2f4ba19ce
Jslb128 <jesulbarreto@gmail.com>
2026-08-26 19:14:08 +0100

    wofi tweaks and some more

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Themes
-text='#c6d0f5'
-red='#e78284'
-yellow='#e5c890'
-blue='#8caaee'
-teal='#81c8be'
-green='#a6d189'
+text='#d8dee9'
+red='#bf616a'
+yellow='#ebcb8b'
+blue='#88c0d0'
+teal='#81a1c1'
+green='#a3be8c'
 
 # install acpi
 date_formatted=$(date +'%d/%m/%Y %H:%M')
@@ -21,7 +21,7 @@ aud_color=$text
 linux_version=$(uname -r | cut -d '-' -f1)
 
 # Get battery info
-bat=$(acpi -b | awk -F', ' '{print $2}')
+bat=$(acpi | awk -F', ' '{print $2}')
 bun=$(brightnessctl -m | cut -d ',' -f4)
 bat_status=$(acpi -b | awk -F' ' '{print $3}' | cut -b 1)
 bat_color=$red
@@ -40,7 +40,6 @@ fi
 
 echo \
 "<span foreground='$text'>$linux_version</span>" \
-"<span foreground='$text'>|</span>" \
 \
 "<span foreground='$aud_color'>($aud_output)</span>" \
 "<span foreground='$text'>vol:</span>" \
@@ -53,5 +52,4 @@ echo \
 "<span foreground='$teal'>$bat</span>" \
 "<span foreground='$bat_color'>$bat_status</span>" \
 \
-"<span foreground='$text'>|</span>" \
-"<span foreground='$text'>$date_formatted </span>"
+"<span foreground='$yellow'>$date_formatted </span>"
--- scripts/batNotify.sh
+++ scripts/batNotify.sh
@@ -3,8 +3,8 @@
 export XDG_RUNTIME_DIR="/run/user/$(id -u)"
 export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
 
-Ulimit=80
-Llimit=25
+Ulimit=84
+Llimit=24
 bat=$(acpi -b | awk '{print $4}' | tr -d '%,')
 bat_status=$(acpi -b |  awk '{print $3}' | tr -d ',')
 if [[ "$bat_status" == "Charging" && "$bat" > "$Ulimit" ]]; then
